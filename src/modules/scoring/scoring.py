4ebd252172346fba164413673c824e8adbc85de7
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2025-02-03 19:12:44 -0500

    touch score/scoring.py

---
+++ src/modules/scoring/scoring.py
@@ -0,0 +1,11 @@
+"""
+scoring.py
+
+TODO: Write module docstring
+"""
+
+import sys
+
+if __name__ == "__main__":
+    print("This module,", __file__, ", is not meant to be run as a script.")
+    sys.exit(0)
